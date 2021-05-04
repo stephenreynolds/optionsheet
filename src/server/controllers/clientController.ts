848e332aa4bb9468ef2ba0e016139610b5eca336
Stephen Reynolds <git@stephenreynolds.dev>
2021-05-03 21:53:44 -0400

    Fix prod client route

--- src/server/controllers/clientController.ts
+++ src/server/controllers/clientController.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import path from "path";
 import config from "../config";
 
 export const getApp = async (request: Request, response: Response) => {
   if (config.isProduction) {
-    response.sendFile(path.resolve("index.html"));
+    response.sendFile(path.resolve(__dirname, "public/index.html"));
   } else {
     response.redirect(
       `http://${config.host}:${config.devPort}${request.originalUrl}`
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -3,9 +3,9 @@ import { getApp } from "./controllers/clientController";
 import {Express} from "express";
 
 export const attachRoutes = (app: Express): void => {
+  app.get("/*", getApp);
   app.post("/api/users", users.createUser);
   app.post("/api/users/check_email", users.checkEmail);
   app.post("/api/users/check_username", users.checkUsername);
   app.post("/api/users/authenticate", users.authenticateUser);
-  app.get("/*", getApp);
 };

39b2f331b10dd91aa9513dcb8e4b9c5f0c311090
Stephen Reynolds <git@stephenreynolds.dev>
2021-05-05 07:11:25 -0400

    Move styles to their own file

---
+++ src/app/components/header/user-menu/user-menu.styles.tsx
@@ -0,0 +1,28 @@
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import styled from "styled-components";
+
+export const StyledDropdownButton = styled(DropdownButton)`
+  button {
+    background-color: transparent;
+    border: none;
+    outline: none !important;
+
+    &:hover,
+    &:focus,
+    &:active {
+      background-color: transparent !important;
+      border: none !important;
+      outline: none !important;
+      box-shadow: none !important;
+    }
+  }
+`;
+
+export const StyledDropdownItem = styled(Dropdown.Item)`
+  color: #000 !important;
+  line-height: 1.5rem;
+
+  &:active {
+    background-color: initial;
+  }
+`;
--- src/app/components/header/user-menu/user-menu.tsx
+++ src/app/components/header/user-menu/user-menu.tsx
@@ -1,34 +1,8 @@
-import { Dropdown, DropdownButton } from "react-bootstrap";
-import styled from "styled-components";
+import { Dropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../../redux/actions/authActions";
 import { getMyInfo } from "../../../redux/selectors/user";
-
-const StyledDropdownButton = styled(DropdownButton)`
-  button {
-    background-color: transparent;
-    border: none;
-    outline: none !important;
-
-    &:hover,
-    &:focus,
-    &:active {
-      background-color: transparent !important;
-      border: none !important;
-      outline: none !important;
-      box-shadow: none !important;
-    }
-  }
-`;
-
-const StyledDropdownItem = styled(Dropdown.Item)`
-  color: #000 !important;
-  line-height: 1.5rem;
-
-  &:active {
-    background-color: initial;
-  }
-`;
+import { StyledDropdownButton, StyledDropdownItem } from "./user-menu.styles";
 
 const UserMenu = (): JSX.Element => {
   const user = useSelector((state) => getMyInfo(state));
@@ -40,8 +14,13 @@ const UserMenu = (): JSX.Element => {
 
   return (
     <>
-      <StyledDropdownButton menuAlign="right" title={user.username ? user.username : ""}>
-        <StyledDropdownItem href={`/${user.username}`}>Profile</StyledDropdownItem>
+      <StyledDropdownButton
+        menuAlign="right"
+        title={user.username ? user.username : ""}
+      >
+        <StyledDropdownItem href={`/${user.username}`}>
+          Profile
+        </StyledDropdownItem>
         <StyledDropdownItem href="/settings">Settings</StyledDropdownItem>
         <Dropdown.Divider />
         <StyledDropdownItem onClick={onLogout}>Sign out</StyledDropdownItem>

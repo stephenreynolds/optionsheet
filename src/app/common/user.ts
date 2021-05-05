4431e96449f59267d03b11b45fb627a84eca699b
Stephen Reynolds <git@stephenreynolds.dev>
2021-05-05 07:26:42 -0400

    Remove dead code

--- src/app/common/user.ts
+++ src/app/common/user.ts
@@ -8,18 +8,3 @@ export interface Credentials {
   username: string;
   password: string;
 }
-
-export const validateUsername = (username: string): boolean => {
-  return username.length > 0;
-};
-
-export const validateEmail = (email: string): boolean => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-};
-
-export const validatePassword = (password: string): boolean => {
-  const minLength = 8;
-  const regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/;
-  return regex.test(password) && password.length >= minLength;
-};

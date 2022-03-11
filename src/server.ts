faca792965ef7ffbef6698340cdc88a48f8a4443
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-11 01:12:24 -0500

    feat: rate limit up to 100 request per minute

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import config from "./config";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100
+});
+
+// Middleware
+app.use(limiter);
 
 app.listen(config.port, config.host, () => {
   console.log(`Server listening on ${config.host}:${config.port}`);

e47c2e5de9bf2abbdbab5adff650f0a943a3bee0
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-11 01:43:03 -0500

    test: remove app.test.ts

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,17 @@ import compression from "compression";
 import cors from "cors";
 import express from "express";
 import rateLimit from "express-rate-limit";
-import config from "./config";
 
 const app = express();
 
 const limiter = rateLimit({
-  windowMs: config.rateLimit.windowMs,
-  max: config.rateLimit.max
+  windowMs: 60 * 100, // 1 minute
+  max: 100
 });
 
 // Middleware
 app.use(limiter);
 app.use(compression());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
 
 export default app;
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
 export default {
   port: Number(process.env.PORT || 4001),
   host: process.env.HOST || "localhost",
-  isProduction: process.env.NODE_ENV === "production",
-  rateLimit: {
-    windowMs: 60 * 100, // 1 minute
-    max: 100
-  }
+  isProduction: process.env.NODE_ENV === "production"
 };

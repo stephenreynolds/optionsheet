1445ee4d35db5c654b087e9fe6a1445e704ff025
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-11 00:54:53 -0500

    add collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
 };

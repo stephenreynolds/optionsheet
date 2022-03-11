1ad6a0d6b13f6df6b14240d5ffe77f5e2bc2064e
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-11 02:05:23 -0500

    refactor: change default port to 5000

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  port: Number(process.env.PORT || 4001),
+  port: Number(process.env.PORT || 5000),
   host: process.env.HOST || "localhost",
   isProduction: process.env.NODE_ENV === "production"
 };

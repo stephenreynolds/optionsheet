f6acf22278713166a7eabf10b66aa1857ac42ddd
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-12 00:28:07 -0500

    refactor: remove default secret

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export default {
   port: Number(process.env.PORT || 5000),
   host: process.env.HOST || "localhost",
   isProduction: process.env.NODE_ENV === "production",
-  secret: process.env.SECRET || "optionsheet"
+  secret: process.env.SECRET
 };

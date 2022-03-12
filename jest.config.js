0a30a96692cc5ee199abd188d29388ee41270457
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-12 00:49:51 -0500

    fix: only look for tests in src

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,12 @@ module.exports = {
     "\\.(gql|graphql)$": "@jagi/jest-transform-graphql"
   },
   collectCoverageFrom: [
-    "**/*.{js,ts}",
-    "!**/node_modules/**",
-    "!**/vendor/**"
+    "src/**/*.{js,ts}"
+  ],
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "build",
+    "jest.config.js",
+    "webpack.config.js"
   ]
 };

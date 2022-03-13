1ac76c6aa11650b9310aa7bba00912d920325295
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-13 01:47:39 -0500

    refactor: remove unused role check code

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
-import HttpStatus from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
 import config from "../config";
-import { getRepository } from "typeorm";
-import { User } from "../data/entities/user";
+import { sendError } from "../errorResponse";
 
 export const verifyJwtToken = (
   request: Request,
@@ -13,53 +12,17 @@ export const verifyJwtToken = (
   const token = request.headers["x-access-token"];
 
   if (!token) {
-    return response.status(HttpStatus.UNAUTHORIZED).send("Requires authentication");
+    sendError(request, response, StatusCodes.UNAUTHORIZED, "Requires authentication");
+    return;
   }
 
   jwt.verify(token, config.secret, (error, decoded) => {
     if (error) {
-      return response.sendStatus(HttpStatus.UNAUTHORIZED);
+      sendError(request, response, StatusCodes.UNAUTHORIZED, "Requires authentication");
+      return;
     }
 
     request.body.userId = decoded.id;
     next();
-  });
-};
-
-export const isAdmin = (
-  request: Request,
-  response: Response,
-  next
-) => {
-  const userRepository = getRepository(User);
-  userRepository.findOne(request.body.userId).then((user) => {
-    const admin = user.roles.find((role) => role.name === "admin");
-    if (admin) {
-      next();
-      return;
-    }
-
-    response.status(HttpStatus.UNAUTHORIZED).send("Requires admin role");
-    return;
-  });
-};
-
-export const isModerator = (
-  request: Request,
-  response: Response,
-  next
-) => {
-  const userRepository = getRepository(User);
-  userRepository.findOne(request.body.userId).then((user) => {
-    const moderator = user.roles.find(
-      (role) => role.name === "moderator" || role.name === "admin"
-    );
-    if (moderator) {
-      next();
-      return;
-    }
-
-    response.status(HttpStatus.UNAUTHORIZED).send("Requires moderator role");
-    return;
   });
 };

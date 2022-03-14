c3492ab286b9bba6d80bc4a0d104d65ffb2aafd0
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-14 12:15:55 -0400

    test: respond with error 429 if rate limit exceeded

---
+++ src/app.test.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import app from "./app";
+import mockDataService from "./data/mockdb/mockDataService";
+import routes from "./routes";
+
+beforeAll(() => {
+  app.use(mockDataService, routes);
+});
+
+describe("when rate limit exceeded", () => {
+  it("should respond with 429 status code", async () => {
+    let response;
+
+    for (let i = 0; i <= 1000; ++i) {
+      response = await request(app).get("/").send();
+    }
+
+    expect(response.status).toEqual(429);
+  });
+});

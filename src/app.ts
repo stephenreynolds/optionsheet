79b05782c2731aef0b8f3dccd88da968bb64eb6f
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-17 01:58:48 -0400

    feat: use helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import compression from "compression";
 import cors from "cors";
 import express from "express";
+import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 
@@ -13,6 +14,7 @@ app.use(rateLimit({
 }));
 app.use(compression());
 app.use(cors());
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("tiny"));

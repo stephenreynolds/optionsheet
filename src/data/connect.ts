b5d239a0f4bdcfe79e2f52abf71faca597b4ad8e
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-20 02:53:26 -0400

    fix: log error if database connection fails

--- src/data/connect.ts
+++ src/data/connect.ts
@@ -1,11 +1,18 @@
 import { createConnection } from "typeorm";
+import logger from "../logger";
 import ormConfig from "./ormConfig";
 import { seedData } from "./seed";
 
 const connect = async () => {
-  const connection = await createConnection(ormConfig);
-  await seedData(connection);
-  return connection;
+  try {
+    const connection = await createConnection(ormConfig);
+    await seedData(connection);
+    return connection;
+  }
+  catch (error) {
+    logger.error(error.message);
+    throw error;
+  }
 };
 
 export default connect;

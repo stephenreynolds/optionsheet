c14c9fa465a23e6cb9fb4174b6a7c7390eef8452
Stephen Reynolds <git@stephenreynolds.dev>
2022-03-24 12:57:13 -0400

    chore: use @types/pg

--- src/data/database.ts
+++ src/data/database.ts
@@ -10,7 +10,7 @@ export class Database {
   constructor() {
     this.pool = new Pool({
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: Number(process.env.DB_PORT),
       database: process.env.DB_DATABASE,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
